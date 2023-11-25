3455c46cc131b62b057021412fc2e2f8af8e9c35
Joan <joan040802@gmail.com>
2023-11-25 13:36:49 +0800

    add 215

---
+++ 215_Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,13 @@
+// Solution1 -> priority queue
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for(int n: nums) {
+            pq.push(n);
+        }
+        for(int i = 0; i < k-1; i++)
+            pq.pop();
+        return pq.top();
+    }
+};
