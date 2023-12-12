e5826595882b3871b48b2d2f9b7bbafd17bb4dfc
Joan <joan040802@gmail.com>
2023-12-12 22:02:39 +0800

    42 done

---
+++ 42_Trapping_Rain_Water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    pair<int, int> check(vector<int>& height, int st, int st_h) {
+        int cnt = 0;
+        for(int i = st + 1; i < height.size(); i++) {
+            if(height[i] < st_h)
+	            cnt += st_h - height[i];
+            else 
+                return make_pair(cnt, i); // meet the right bound
+        }
+        if(st_h - 1 >= 0)
+            return check(height, st, st_h-1); // no right bound, height - 1
+        else
+	        return make_pair(cnt, st+1); // not found
+    }
+    int trap(vector<int>& height) {
+        int left = 0, cnt = 0; // left for the index, cnt for the total water
+        while(left < height.size()) {
+            pair<int, int> result = check(height, left, height[left]);
+            cnt += result.first;
+            left = result.second; // the next bound that is higher or equal to the original bound
+        }
+        return cnt;
+    }
+};
