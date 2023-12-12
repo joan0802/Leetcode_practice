cc48f745a2a95529c9d63a2e6086e48301bbcabd
Joan <joan040802@gmail.com>
2023-12-12 23:11:41 +0800

    add 42 another efficient solution

--- 42_Trapping_Rain_Water.cpp
+++ 42_Trapping_Rain_Water.cpp
@@ -1,3 +1,4 @@
+// my solution
 class Solution {
 public:
     pair<int, int> check(vector<int>& height, int st, int st_h) {
@@ -21,5 +22,33 @@ public:
             left = result.second; // the next bound that is higher or equal to the original bound
         }
         return cnt;
+    }
+};
+
+// faster solution
+// time: O(n), memory: O(1)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int lMax = height[0], rMax = height[height.size()-1];
+        int n = height.size();
+        int l = 1, r = n-2, ans = 0;
+        while(l <= r) {
+            if(lMax <= rMax) {
+                if(height[l] < lMax) 
+                    ans += max(0, min(lMax, rMax) - height[l]);
+                else 
+                    lMax = height[l];
+                l++;
+            }
+            else {
+                if(height[r] < rMax)
+                    ans += max(0, min(lMax, rMax) - height[r]);
+                else 
+                    rMax = height[r];
+                r--;
+            }
+        }
+        return ans;
     }
 };
